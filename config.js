eb07d17ede2470c669db7efc61f7983356d602ab
11vincentnguyen <98414710+11vincentnguyen@users.noreply.github.com>
2022-01-26 00:09:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
     titleWeb: "Xin lỗi Ichill",
     introTitle: 'lchill',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh làm cái này chỉ dể xin lỗi em vì đã để em phải chờ anh.`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
